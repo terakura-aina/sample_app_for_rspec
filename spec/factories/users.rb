9ed5c48494a86653224014b1a13d3db06150cfb9
terakura aina <aicaierwei@gmail.com>
2020-11-20 18:18:07 +0900

    Fix 修正内容

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "tester#{n}@example.com" }
+    sequence(:email, "tester_1@example.com")
     password { 'foobar' }
     password_confirmation { 'foobar' }
   end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,32 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe 'validation' do
-    it 'is valid with all attributes' do end
+    it '全て入力されている時有効であること' do
+      task = build(:task)
+      expect(task).to be_valid
+      expect(task.errors).to be_empty
+    end
 
     it 'titleが空の時無効であること' do
-      task = FactoryBot.build(:task, title: nil)
-      task.valid?
-      expect(task.errors[:title]).to include("can't be blank")
+      task_without_title = build(:task, title: nil)
+      expect(task_without_title).to be_invalid
+      expect(task_without_title.errors[:title]).to eq ["can't be blank"]
     end
 
     it 'statusが空の時無効であること' do
-      task = FactoryBot.build(:task, status: nil)
-      task.valid?
-      expect(task.errors[:status]).to include("can't be blank")
+      task = build(:task, status: nil)
+      expect(task).to be_invalid
+      expect(task.errors[:status]).to eq ["can't be blank"]
     end
 
     it 'titleが同じ時無効であること' do
-      FactoryBot.create(:task, title: 'test')
-      task = FactoryBot.build(:task, title: 'test')
-      task.valid?
-      expect(task.errors[:title]).to include('has already been taken')
+      task = create(:task)
+      task_with_duplicated_title = build(:task, title: task.title)
+      expect(task_with_duplicated_title).to be_invalid
+      expect(task_with_duplicated_title.errors[:title]).to eq ["has already been taken"]
     end
 
     it 'titleが違う時有効であること' do
-      FactoryBot.create(:task, title: 'test')
-      task = FactoryBot.build(:task, title: 'test2')
-      task.valid?
-      expect(task).to be_valid
+      task = create(:task)
+      task_with_another_title = build(:task, title: 'another_title')
+      expect(task_with_another_title).to be_valid
+      expect(task_with_another_title.errors).to be_empty
     end
   end
 end
