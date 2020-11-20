e917fc7b77e5bf05a5f243fc0d626ffd258f6ebe
terakura aina <aicaierwei@gmail.com>
2020-11-20 17:38:40 +0900

    taskモデルのバリデーションに関するテスト作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :task do
-    
+    title { "test" }
+    status { "todo" }
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is valid with all attributes' do end
+
+    it 'titleが空の時無効であること' do
+      task = FactoryBot.build(:task, title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+
+    it 'statusが空の時無効であること' do
+      task = FactoryBot.build(:task, status: nil)
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+
+    it 'titleが同じ時無効であること' do
+      FactoryBot.create(:task, title: 'test')
+      task = FactoryBot.build(:task, title: 'test')
+      task.valid?
+      expect(task.errors[:title]).to include('has already been taken')
+    end
+
+    it 'titleが違う時有効であること' do
+      FactoryBot.create(:task, title: 'test')
+      task = FactoryBot.build(:task, title: 'test2')
+      task.valid?
+      expect(task).to be_valid
+    end
+  end
 end
