2c6504a6b52569753b4707ed0c3cbd37d33979e0
terakura aina <aicaierwei@gmail.com>
2020-11-24 23:38:35 +0900

    システムスペックテストの作成

---
+++ spec/support/login_support.rb
@@ -0,0 +1,13 @@
+module LoginSupport
+  def sign_in_as(user)
+    visit root_path
+    click_link "Login"
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'foobar'
+    click_button 'Login'
+  end
+end
+
+RSpec.configure do |config|
+  config.include LoginSupport
+end
